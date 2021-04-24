b48fb3d55ff78e2e9337930497c9b1c78b764186
Nicholas Vincent <nickmvincent@gmail.com>
2021-04-24 10:43:41 -0500

    update to formatter

--- scripts/format_queries_24Apr2021.py
+++ scripts/format_queries_24Apr2021.py
@@ -2,6 +2,7 @@ import glob
 
 out = ""
 path = 'search_queries/prepped/hw/*'
+new_path = 'search_queries/prepped/hw_nice/'
 
 paths = glob.glob(path)
 print(paths)
@@ -14,6 +15,10 @@ for x in paths:
         else:
             out += line.strip("\n")
 
-    new_name = x.replace(' ', '_').strip('.txt') + '_nice.txt' 
+    parts = x.split('/')
+    print(parts)
+    name = parts[-1]
+    new_name = new_path + name.replace(' ', '_')
+    #new_name = x.replace(' ', '_').strip('.txt') + '_nice.txt' 
     with open(new_name, 'w') as f:
         f.write(out)
