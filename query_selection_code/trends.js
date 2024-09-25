65bd621e7ed716603787ffcdade8152019747318
Nathan TeBlunthuis <nathanteblunthuis@gmail.com>
2024-09-25 10:14:13 -0700

    bugfix.

--- query_selection_code/trends.js
+++ query_selection_code/trends.js
@@ -8,9 +8,6 @@ const queryCats = [];
 
 (async () => {
 
-    const text = fs.readFileSync(stem_file, "utf-8");
-    const targets = text.split("\n").filter(Boolean); // removes empty strings
-
     await googleTrends.dailyTrends({
             trendDate: today,
             geo: 'US',
@@ -29,6 +26,7 @@ const queryCats = [];
         .catch((err) => {
             console.log(err);
         });
+
     const jsonPath = `search_queries/script_generated/${niceDateStr}_metadata.json`
     fs.writeFile(jsonPath, JSON.stringify(queryCats), 'utf8', () => console.log(`Wrote to ${jsonPath}`));
 })();
