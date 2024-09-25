871dbaed9ba08d32d80666c982b8fbae2adb83ae
Nathan TeBlunthuis <nathanteblunthuis@gmail.com>
2024-09-25 10:08:35 -0700

    remove stems for greggle project.

--- query_selection_code/trends.js
+++ query_selection_code/trends.js
@@ -1,8 +1,6 @@
 const googleTrends = require('google-trends-api');
 const fs = require('fs');
 
-
-
 const today = new Date();
 const niceDateStr = today.toString().replace(/:/g, '-'); // (nice for Windows filesystem)
 
@@ -10,7 +8,6 @@ const queryCats = [];
 
 (async () => {
 
-    const stem_file = `search_queries/prepped/covid_stems/0.txt`;
     const text = fs.readFileSync(stem_file, "utf-8");
     const targets = text.split("\n").filter(Boolean); // removes empty strings
 
@@ -32,24 +29,6 @@ const queryCats = [];
         .catch((err) => {
             console.log(err);
         });
-    for await (const stem of targets) {
-        await googleTrends.relatedQueries({
-            keyword: stem
-        })
-        .then((results) => {
-            const jsonPath = `search_queries/script_generated/relatedQueries_raw_${stem}_${niceDateStr}.json`;
-            fs.writeFile(jsonPath, results, 'utf8', () => console.log(`Wrote to ${jsonPath}`));
-            queryCats.push({
-                name: `relatedQueries_${stem}`,
-                date: today,
-                raw: jsonPath
-            });
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-    }
-
     const jsonPath = `search_queries/script_generated/${niceDateStr}_metadata.json`
     fs.writeFile(jsonPath, JSON.stringify(queryCats), 'utf8', () => console.log(`Wrote to ${jsonPath}`));
 })();
