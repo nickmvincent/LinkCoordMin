928dd56b5ca2f1fcef7532272aa3f8ef37925df4
Nathan TeBlunthuis <nathanteblunthuis@gmail.com>
2024-09-25 19:30:58 -0700

    create json files for fake trending queries with "reddit" appended.

--- query_selection_code/trends.js
+++ query_selection_code/trends.js
@@ -22,6 +22,20 @@ async function get_trends_schema() {
     };
 };
 
+function replaceNameValue(obj) {
+    const prop_names = Object.getOwnPropertyNames(obj)
+    for (let idx in prop_names) {
+	name = prop_names[idx]
+	if (name == 'query') {
+	    obj.query = obj.query + " Reddit";
+	}
+	// If the value is an object or array, recurse into it
+	if (typeof obj[name] === "object" && obj[name] !== null) {
+            replaceNameValue(obj[name]);
+	}
+    }
+}
+
 async function get_category_trends(name, id){
     try {
 	results = await googleTrends.dailyTrends({
@@ -29,15 +43,28 @@ async function get_category_trends(name, id){
             geo: 'US',
 	    category: id
 	});
-	console.log(name)
+
 	const jsonPath = `search_queries/script_generated/${name}_raw_${niceDateStr}.json`;
+	const redditJsonPath = `search_queries/script_generated/${name}_reddit_raw_${niceDateStr}.json`;
 	fs.writeFile(jsonPath, results, 'utf8', () => console.log(`Wrote to ${jsonPath}`));
-	return({
-            name: name,
-            date: today,
-            raw: jsonPath,
-	    id: id
-	});
+	reddit_results = JSON.parse(results);
+	replaceNameValue(reddit_results);
+	fs.writeFile(redditJsonPath, JSON.stringify(reddit_results), 'utf8', () => console.log(`Wrote to ${redditJsonPath}`));
+	console.log(reddit_results);
+
+	return([
+	    {
+		name: name,
+		date: today,
+		raw: jsonPath,
+		id: id
+	    },
+	    {
+		name: name + " reddit",
+		date: today,
+		raw: redditJsonPath,
+		id: id
+	    }]);
     } catch (err) {
         console.log(err);
     };
@@ -47,16 +74,16 @@ queryCats = [];
 (async () => {
     try{
 	jsonData = await get_trends_schema();
-	console.log(jsonData);
-	
-	queryCat = await get_category_trends(jsonData.name.replace(" & ", "_"),
+	queryCatrg = await get_category_trends(jsonData.name.replace(" & ", "_"),
 					 jsonData.id);
-	queryCats.push(queryCat);
+	queryCats.push(queryCatrg[0]);
+	queryCats.push(queryCatrg[1]);
 	for (var idx in jsonData.children){
 	    category = jsonData.children[idx];
-	    queryCat = await get_category_trends(category.name.replace(" & ","_"),
+	    queryCatrg = await get_category_trends(category.name.replace(" & ","_"),
 						 category.id);
-	    queryCats.push(queryCat);
+	    queryCats.push(queryCatrg[0]);
+	    queryCats.push(queryCatrg[1]);
 	};
 
     } catch (error) {
